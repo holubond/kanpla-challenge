284092beedb1b4527ce84789982a28414f0b0763
Ondra Holub <ondrejholub202@gmail.com>
2023-12-26 13:42:15 +0100

    feat: change font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
 import { twMerge } from 'tailwind-merge'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -17,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={twMerge(inter.className, 'bg-[#fbf7ff]')}>{children}</body>
+      <body className={twMerge('font-sans bg-[#fbf7ff]')}>{children}</body>
     </html>
   )
 }
