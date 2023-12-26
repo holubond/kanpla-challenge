999e1267433a941f40ba2ac2712c58c5ce612312
Ondra Holub <ondrejholub202@gmail.com>
2023-12-26 14:46:19 +0100

    feat:minor styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,9 @@ import { Search } from "@/components/Search";
 export default function Home() {
   return (
     <div className="flex justify-center">
-      <Card className="mt-5">
-        <Heading>Select Locations</Heading>
+      <Card className="mt-5 w-5/6 sm:w-1/2">
+        <Heading className="text-center py-4">Select Locations</Heading>
         <Search placeholder="Search location or a group"/>
-
       </Card>
     </div>
   )
