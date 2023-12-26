b2a5d05a8a83f202bc58a933653e29e7ec4b7a1f
Ondra Holub <ondrejholub202@gmail.com>
2023-12-26 14:34:12 +0100

    feat: add search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import { Card } from "@/components/Card";
 import { Heading } from "@/components/Heading";
+import { Search } from "@/components/Search";
 
 export default function Home() {
   return (
     <div className="flex justify-center">
       <Card className="mt-5">
         <Heading>Select Locations</Heading>
+        <Search placeholder="Search location or a group"/>
+
       </Card>
     </div>
   )
---
+++ src/components/Search.tsx
@@ -0,0 +1,20 @@
+import { MagnifyingGlassIcon } from '@heroicons/react/20/solid'
+
+type Props = {
+    placeholder?: string
+}
+
+export function Search({ placeholder }: Props) {
+    return (
+        <div className="relative">
+            <input
+                type="text"
+                className="block w-full rounded-md border border-gray-200 py-2 pl-4 pr-10 text-gray-900  placeholder:text-gray-400"
+                placeholder={placeholder}
+            />
+            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+                <MagnifyingGlassIcon className="h-5 w-5 text-gray-900" aria-hidden="true" />
+            </div>
+        </div>
+    )
+}
