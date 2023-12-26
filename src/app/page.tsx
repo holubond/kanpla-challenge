0752b5593ec6c469e0e76074be4eda1672926be4
Ondra Holub <ondrejholub202@gmail.com>
2023-12-26 13:47:47 +0100

    feat: style heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import { Card } from "@/components/Card";
+import { Heading } from "@/components/Heading";
 
 export default function Home() {
   return (
     <div className="flex justify-center">
       <Card className="mt-5">
-        <h1>hello</h1>
+        <Heading>Select Locations</Heading>
       </Card>
     </div>
   )
---
+++ src/components/Heading.tsx
@@ -0,0 +1,12 @@
+import { ParentProps } from "@/utils/types";
+import { twMerge } from "tailwind-merge";
+
+type Props = ParentProps<{
+    className?: string,
+}>
+
+export function Heading({ children, className }: Props) {
+    return (
+        <h1 className={twMerge("text-2xl font-bold text-gray-900", className)}>{children}</h1>
+    )
+}
