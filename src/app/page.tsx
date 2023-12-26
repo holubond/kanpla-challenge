34e611c734d514d8fe3529098cdbbd3616227eb4
Ondra Holub <ondrejholub202@gmail.com>
2023-12-26 17:17:38 +0100

    feat: use URL search params as a state for the query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import { Card } from "@/components/Card";
 import { Heading } from "@/components/Heading";
-import { Search } from "@/components/Search";
+import { SearchUrlParam } from "./SearchUrlParam";
+
+const URL_PARAM_SEARCH = 'search'
 
 export default function Home() {
   return (
     <div className="flex justify-center">
       <Card className="mt-5 w-5/6 sm:w-1/2">
         <Heading className="text-center py-4">Select Locations</Heading>
-        <Search placeholder="Search location or a group"/>
+        <SearchUrlParam urlParam={URL_PARAM_SEARCH} placeholder="Search for location or a group"/>
       </Card>
     </div>
   )
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,16 +1,22 @@
 import { MagnifyingGlassIcon } from '@heroicons/react/20/solid'
 
 type Props = {
-    placeholder?: string
+    placeholder?: string,
+    value?: string,
+    defaultValue?: string,
+    onInput: (value: string) => void
 }
 
-export function Search({ placeholder }: Props) {
+export function Search({ placeholder, value, defaultValue, onInput }: Props) {
     return (
         <div className="relative">
             <input
                 type="text"
                 className="block w-full rounded-md border border-gray-200 py-2 pl-4 pr-10 text-gray-900  placeholder:text-gray-400"
                 placeholder={placeholder}
+                onInput={(e: React.ChangeEvent<HTMLInputElement>) => onInput(e.target.value)}
+                value={value}
+                defaultValue={defaultValue}
             />
             <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
                 <MagnifyingGlassIcon className="h-5 w-5 text-gray-900" aria-hidden="true" />
